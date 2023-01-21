f160ce2fc24c28ce102ef51ade5ca5e8aed24b58
Xtreme Workplace - Online Marketing & Business <102599667+Xtremeworkplace@users.noreply.github.com>
2023-01-21 13:35:42 +0530

    Add existing file

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          {/* <link rel="shortcut icon" href={favicon} /> */}
+          <link rel="shortcut icon" href="/icon.png" />
+        </Head>
       <body>
         <Main />
         <NextScript />
